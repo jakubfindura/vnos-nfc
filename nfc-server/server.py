24243ee692383910706a754acb2998da54cd92ec
DESKTOP-SNGKH43\jakub <findurajakub@gmail.com>
2017-05-01 01:27:10 +0200

    Revert "LED Blinking when authenticating"
    This reverts commit 15b531fffb2420519f389a85eb45bbf6376ec521.

--- nfc-server/server.py
+++ nfc-server/server.py
@@ -1,17 +1,12 @@
 from logger import LOG
 from keypad import readFromKeyboard
-import RPi.GPIO as GPIO
-from time import sleep
-import threading
-from multiprocessing import Process
 import pyrebase
+import json
+import time
 
 FIFO_FILE = "nfc_fifo.tmp"
+AUDIT_FILE = "arrays.json"
 NUMBER_OF_PIN_ATTEMPTS = 3
-
-LED_WHITE = 19
-LED_RED = 21
-LED_GREEN = 23
 
 
 config = {
@@ -29,55 +24,32 @@ class NfcRecord:
          self.nID = nID
          self.timestamp = timestamp
 
+
+def appendAuditJson(jsonAudit):
+  with open(AUDIT_FILE, mode='r', encoding='utf-8') as feedsjson:
+    feeds = json.load(feedsjson)
+  with open(AUDIT_FILE, mode='w', encoding='utf-8') as feedsjson:
+    feeds['data'].append(jsonAudit)
+    json.dump(feeds, feedsjson)
+
 # Load Firebase APP
-print("A")
 firebase = pyrebase.initialize_app(config)
-print("B")
 db = firebase.database()
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(LED_GREEN,GPIO.OUT)
-GPIO.setup(LED_RED,GPIO.OUT)
-GPIO.setup(LED_WHITE,GPIO.OUT)
-
-def led_coroutine(LED_GPIO, delay, count):
-  print("CORO {}".format(LED_GPIO))
-  if count > 0 :
-    for x in range(1,count+1):
-      GPIO.output(LED_GPIO,True)
-      sleep(delay/1000/2)
-      GPIO.output(LED_GPIO,False)
-      sleep(delay/1000/2)
-  else:
-    while True:
-      GPIO.output(LED_GPIO,True)
-      sleep(delay/1000/2)
-      GPIO.output(LED_GPIO,False)
-      sleep(delay/1000/2)
-
-  GPIO.output(LED_GPIO,False)
-
-def blink(led,delay=200,count=5):
-  t = Process(target=led_coroutine, daemon=True, args=(led,delay,count))
-  t.start()
-  
+LOG.debug("Firebase loaded")
 
 # Block until writer finishes...
 while True:
-    white = Process(target=led_coroutine, daemon=True, args=(LED_WHITE,300,0))
-    print("Waiting...")
+    LOG.debug("Waiting for communication...")
     with open(FIFO_FILE, 'r') as f:
         data = f.read()
 
-    white.start()
 
-    LOG.info("Loaded string from NFC:{}".format(data))
+    LOG.debug("Loaded string from NFC:{}".format(data))
     LOG.debug("Spliting loaded string to array")
-    print(data)
 
     # Split data into an array
     array = [x for x in data.split()]
-    print(array)
+
     
     nfcRecord = NfcRecord(''.join(array[:2]), \
                           ''.join(array[-2:]), \
@@ -85,40 +57,46 @@ while True:
                           ''.join(array[3:-6]), \
                           int(''.join(array[19:-2]),16))
 
-    users = db.child("users").get()
-    user = db.child("users").order_by_child("nid").equal_to(nfcRecord.nID).get().val()
+    LOG.debug("NID received: {}".format(nfcRecord.nID))
 
-    for x in users.val():
-        print(users.val()[x]["nid"])
-        print(nfcRecord.nID)
-        if(users.val()[x]["nid"] == nfcRecord.nID):
-            userDictionary = users.val()[x]
-            print("DISPLEJ - Zadaj PIN")
-            LOG.info("ID is in database.")
-            LOG.info("Waiting for keyboard INPUT from keyboard")
+    user = db.child("users").order_by_child("nid").equal_to(nfcRecord.nID).get()
+    if(user.each() == []):
+        print("DISPLEJ - Pouzivatel neexistuje!")
+        LOG.debug("User not found in database!")
+    else:
+      userValues = user.each()[0].val()
 
-            for i in range(NUMBER_OF_PIN_ATTEMPTS):
-                LOG.info("Loaded PIN")
-                if(userDictionary["pin"] == readFromKeyboard()):
-                    LOG.info("Authorization successful, User:{}".format(userDictionary["name"]))
-                    print("DISPLEJ - Autorizacia uspesna!")
-                    blink(LED_GREEN,300,5)
-                    break
-                else:
-                    LOG.info("Authorization unsuccessful User: {}, Attempt: {}/{}".format(userDictionary["name"], \
-                                                                                              i+1, \
-                                                                                              NUMBER_OF_PIN_ATTEMPTS))
-                    print("DISPLEJ - Autorizacia neuspesna! Pokus {} z {}".format(i+1, \
-                                                                                  NUMBER_OF_PIN_ATTEMPTS))
-                    blink(LED_RED,200,3)
+      outputJson = {'Timestamp': datetime.datetime.now().isoformat(),
+                    'Username': userValues['name'],
+                    'E-Mail': userValues['email'],
+                    'FirebaseID': user.each()[0].key(),
+                    'Action':'False'}
 
+      print("DISPLEJ - Zadaj PIN")
+      LOG.debug("ID is in database.")
+      LOG.debug("Waiting for keyboard INPUT...")
+      for i in range(NUMBER_OF_PIN_ATTEMPTS):
+        loadedPin = readFromKeyboard()
+        LOG.debug("Loaded PIN:{}".format(loadedPin))
 
+        if(userValues["pin"] == loadedPin):
+          outputJson["Action"] = "True"
+          appendAuditJson(outputJson)
+          LOG.debug("Authorization successful, User:{}".format(userValues["name"]))
+          print("DISPLEJ - Autorizacia uspesna!")
+          break
         else:
-            LOG.info("User not found in database!")
-            print("DISPLEJ - Pouzivatel neexistuje!")
+          appendAuditJson(outputJson)
+          LOG.debug("Authorization unsuccessful User: {}, Attempt: {}/{}".format(userValues["name"], \
+                                                                                    i+1, \
+                                                                                    NUMBER_OF_PIN_ATTEMPTS))
+          print("DISPLEJ - Autorizacia neuspesna! Pokus {} z {}".format(i+1, \
+                                                                      NUMBER_OF_PIN_ATTEMPTS))
 
-    white.terminate()
-    GPIO.output(LED_WHITE,False)
+    LOG.debug("Authorization finished. User not logged in!!!")
+
+
+
 
 
 
